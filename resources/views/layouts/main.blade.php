13ae63b6804a458bec8eb0035935ea86eee743a2
Roberta Assis <robertaassis@discente.ufg.br>
2021-08-10 19:31:39 -0300

    Flash Messages

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -40,7 +40,17 @@
                 </div>
             </nav>
         </header>
-        @yield('content')
+        <main>
+            <div class="container-fluid">
+                <div class="row">
+                    @if(session('msg'))
+                    <p class="msg">{{session('msg')}}</p>
+                    @endif
+                    @yield('content')
+                </div>
+            </div>
+        </main>
+       
       <footer> 
           <p>LARAVEL TESTE &copy; 2021</p>
       </footer>
