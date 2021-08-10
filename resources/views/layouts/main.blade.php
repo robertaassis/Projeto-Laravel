826f25b02f3f8b331d87e541f1dc615b0d5203e9
Roberta Assis <robertaassis@discente.ufg.br>
2021-08-10 18:14:29 -0300

    Adicionando código  Home e CSS

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,7 +13,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
         {{-- css do programa --}}
-        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+        <link rel="stylesheet" href="{{asset('css/styles.css')}}">
         <script src="{{asset('js/script.js')}}"></script>
     </head>
     <body>
